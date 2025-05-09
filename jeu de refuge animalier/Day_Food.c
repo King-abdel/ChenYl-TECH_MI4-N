a29af5fe92c6aff5085aa7e993f21316983bcdb1
deusyg <moulayyoussef.ghazi@gmail.com>
2025-05-09 18:59:00 +0200

    Create Day_Food.c

---
+++ jeu de refuge animalier/Day_Food.c
@@ -0,0 +1,32 @@
+
+
+void DAY_FOOD() {
+    int annee_courante;
+    printf("\n Entrez l'année actuelle : ");
+    scanf("%d", &annee_courante);
+
+    float total_croquettes_kg = 0.0;
+
+    for (int i = 0; i < nb_animal; i++) {
+        Animal a = refuge[i];
+        int age = annee_courante - a.annee;
+
+        switch (a.espece) {
+            case hamsters:
+                total_croquettes_kg += 0.02; // 20g
+                break;
+            case autruche:
+                total_croquettes_kg += 2.5;   // 2.5kg
+                break;
+            case chats:
+            case chien:
+                if (age < 2) {
+                    total_croquettes_kg += 0.5; // 500g
+                } else {
+                    total_croquettes_kg += a.poids * 0.10; // 10% du poids
+                }
+                break;
+        }
+    }
+
+    printf("\n🍽️  Quantité totale de croquettes nécessaire aujourd'hui : %.2f kg\n", total_croquettes_kg);
