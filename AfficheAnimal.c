1f94d5fe567f3a1b8f9eab5377f4e05579982cdd
zain <azamzain62@gmail.com>
2025-05-11 01:47:05 +0200

    Update AfficheAnimal.c

--- AfficheAnimal.c
+++ AfficheAnimal.c
@@ -1,28 +1,37 @@
 #include "FichierStructure.h"
 #include "FichierSource.h"
 
+// Fonction pour afficher les informations d’un animal
 void afficheAnimale(Animal a, int choix){
-	printf("-----\n");
-	printf("Nom : %s\n", a.nom);
-	printf("id : %d\n", a.id);
-	printf("annee : %d\n", a.annee);
-	printf("poids : %.2f kg\n", a.poids);
-	switch(a.espece){
+    printf("-----\n");  
+    printf("Nom : %s\n", a.nom);         // Affichage du nom de l’animal
+    printf("id : %d\n", a.id);           // Affichage de l’id
+    printf("annee : %d\n", a.annee);     // Affichage de l’année de naissance
+    printf("poids : %.2f kg\n", a.poids); // Affichage du poids
+
+    // Affichage de l'espèce 
+    switch(a.espece){
         case 1 :
-       		printf("Espece : chien \n");
-       		break;
+            printf("Espece : chien \n");
+            break;
         case 2 :
-       		printf("Espece : chat \n");
-      		break;
+            printf("Espece : chat \n");
+            break;
         case 3 :
-        	printf("Espece : Hamster \n");
-        	break;
+            printf("Espece : Hamster \n");
+            break;
         case 4 :
-        	printf("Espece : Autruche \n");
-        	break;
-	}
-	if(choix==1){  
-        printf("description : %s\n", a.descrip);
-	}
-    printf("-----\n");   
+            printf("Espece : Autruche \n");
+            break;
+        default:
+            printf("Espece : inconnue \n"); // Sécurité si valeur inattendue
+            break;
     }
+
+    // Affichage aussi la description
+    if(choix == 1){  
+        printf("description : %s\n", a.descrip);
+    }
+
+    printf("-----\n");  
+}
