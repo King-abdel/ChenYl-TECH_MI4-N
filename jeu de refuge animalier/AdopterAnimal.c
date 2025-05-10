841fcec12acd41596892e4a3e397a3e3077f9cf1
zain <azamzain62@gmail.com>
2025-05-11 00:44:44 +0200

    Update AdopterAnimal.c

--- jeu de refuge animalier/AdopterAnimal.c
+++ jeu de refuge animalier/AdopterAnimal.c
@@ -2,29 +2,29 @@
 #include "FichierSource.h"
 
 void adopter_animal() {
-    int id;
+    int id;		//supprimer un animal selon son id unique
     do{
 	    printf("ID de l'animal à adopter : ");
 	    scanf("%d", &id);
 	    while (getchar() != '\n');
     }while(id<=0);
 
-    int trouvé = 0;
+    int trouvé = 0; // marqeur pour savoir si l'animal a été trouvé
     for (int i = 0; i < nb_animal; i++) {   // parcours tout les animaux
         if (refuge[i].id == id) {   
             trouvé = 1;                    
             printf("Animal trouvé :\n");
             afficheAnimale(refuge[i], 1);
             for (int j = i; j < nb_animal - 1; j++) {    
-                refuge[j] = refuge[j + 1];                        // on réajuste les indices du tableaux 
+                refuge[j] = refuge[j + 1];       // on réajuste les indices du tableaux 
             }
-            nb_animal--;
+            nb_animal--;			// enlever la place de l'animal supprimé
             printf("Animal avec ID %d adopté avec succès.\n", id);
             break;
         }
     }
 	
-    if (trouvé==0) {
+    if (trouvé==0) {		// traite le cas où l'id n'est valide
         printf("Aucun animal trouvé avec l'ID %d.\n", id);
     }
     
