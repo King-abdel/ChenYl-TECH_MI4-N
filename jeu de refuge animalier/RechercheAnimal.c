2bf77466f7d23a235850ca134d50dc0e08ca5fa2
zain <azamzain62@gmail.com>
2025-05-11 01:58:03 +0200

    Update RechercheAnimal.c

--- jeu de refuge animalier/RechercheAnimal.c
+++ jeu de refuge animalier/RechercheAnimal.c
@@ -1,22 +1,24 @@
 #include "FichierStructure.h"
 #include "FichierSource.h"
 
-
+// Fonction permettant de rechercher des animaux selon plusieurs critères (nom, espèce, âge)
 void rechercher_animaux() {
-    Animal a;
-    char nom[MAX];
-    int filtre_espece, age_type;
-    int critere_nom = 0, critere_espece = 0, critere_age = 0;
+    Animal a;                      // Variable  pour parcourir le tableau
+    char nom[MAX];                // Nom à saisir
+    int filtre_espece, age_type;  // Variables pour filtrer espèce et type d’âge
+    int critere_nom = 0, critere_espece = 0, critere_age = 0; // Variable pour savoir les filtres saisir
 
+    // Demande si l'utilisateur veut filtrer par nom
     printf("Rechercher par nom ? (1:Oui / 0:Non) : ");
     scanf("%d", &critere_nom);
-    while (getchar() != '\n');
+    while (getchar() != '\n');  // supprime '\n' du tampon
     if (critere_nom == 1) {
         printf("Entrez le nom : ");
-        fgets(nom, MAX,stdin);
-        nom[strcspn(nom, "\n")] = '\0';
+        fgets(nom, MAX, stdin);                     // lire une chaîne avec des espaces
+        nom[strcspn(nom, "\n")] = '\0';             // enlève du \n dans la chaîne
     }
 
+    // Demande si l'utilisateur veut filtrer par espèce
     printf("Rechercher par espèce ? (1:Oui / 0:Non) : ");
     scanf("%d", &critere_espece);
     while (getchar() != '\n');
@@ -26,6 +28,7 @@ void rechercher_animaux() {
         while (getchar() != '\n');
     }
 
+    // Demande si l'utilisateur veut filtrer par âge
     printf("Rechercher par type d'âge ? (1:Oui / 0:Non) : ");
     scanf("%d", &critere_age);
     while (getchar() != '\n');
@@ -35,28 +38,39 @@ void rechercher_animaux() {
         while (getchar() != '\n');
     }
 
-    int trouve = 0;
-    int annee_courante = 2025;
+    int trouve = 0;                // Marqueur pour savoir si un animal a été trouvé
+    int annee_courante = 2025;     // Année actuelle pour le calcul de l’âge
+
+    // Parcours de tous les animaux du refuge
     for (int i = 0; i < nb_animal; i++) {
         a = refuge[i];
-        int age = annee_courante - a.annee;
+        int age = annee_courante - a.annee;  // Calcul de l’âge de l’animal
 
-        int repere = 1;
+        int repere = 1;  //vérifie si l’animal correspond à tous les filtres
+
+        // Filtre par nom 
         if (critere_nom == 1 && strcmp(a.nom, nom) != 0)
             repere = 0;
+
+        // Filtre par espèce
         if (critere_espece == 1 && a.espece != filtre_espece)
             repere = 0;
+
+        // Filtre par type d’âge 
         if (critere_age == 1) {
             if ((age_type == 1 && age >= 2) || (age_type == 2 && age <= 10))
                 repere = 0;
         }
 
+        // Si l’animal vérifie les filtres, on l’affiche
         if (repere == 1) {
-            afficheAnimale(a, 1);
+            afficheAnimale(a, 1);  // Affichage des caractéristiques de l'animal
             trouve = 1;
         }
     }
-    if (trouve==0){
+
+    // le cas où aucun animal n’a été trouvé
+    if (trouve == 0){
         printf("Aucun animal ne correspond aux critères.\n");
     }
 }
