58461235dfbad767648d4a0f7dfa7926db9231e6
zain <azamzain62@gmail.com>
2025-05-11 00:28:40 +0200

    Update AjouterAnimal.c

--- jeu de refuge animalier/AjouterAnimal.c
+++ jeu de refuge animalier/AjouterAnimal.c
@@ -1,65 +1,52 @@
 #include "FichierStructure.h"
 
-void afficheAnimale(Animal b){
-	printf("Nom : %s\n", b.nom);
-	printf("id : %d\n", b.id);
-	printf("annee : %d\n", b.annee);
-	printf("poids : %f\n", b.poids);
-	switch(b.espece){
-        case 1 :
-			printf("Espece : chien \n");
-			break;
-        case 2 :
-			printf("Espece : chat \n");
-			break;
-        case 3 :
-			printf("Espece : Hamster \n");
-			break;
-        case 4 :
-			printf("Espece : Autruche \n");
-			break;
-	}
-	printf("description : %s\n", b.descrip);
-}
-
 void ajouter_animal(){ 
     if (nb_animal >= NbAnimal) {
     	printf("Capacité maximale atteinte.\n");
-    
 	}
 	else{	
-	Animal a;
-	a.id = prochain_id++;
-	
-	printf("Nom : ");
-	
-	scanf("%s", a.nom);
-	
-	do{
-	printf("Espèce (1:Chien, 2:Chat, 3:Hamster, 4:Autruche) : ");
-	scanf("%d", (int*)&a.espece);
-	}while(a.espece<1 || a.espece>4);
+		Animal a;
+		a.id = prochain_id++;
+		
+		printf("Nom : \n");
+		fgets(a.nom, MAX,stdin);                 // lire avec possibilté de mettre des espaces
+		a.nom[strcspn(a.nom, "\n")] = '\0';     // enlever le \n de la chaîne nom 
 
 	do{
-	printf("Année de naissance : ");
-	scanf("%d", &a.annee);
-	}while(a.annee<0);
+		printf("Espèce (1:Chien, 2:Chat, 3:Hamster, 4:Autruche) : \n");
+		scanf("%d", &a.espece);
+		while (getchar() != '\n');              // supprimer le \n dans le tampon
+	} while(a.espece<1 || a.espece>4);
+
+	do{
+		printf("Année de naissance : \n");
+		scanf("%d", &a.annee);
+		while (getchar() != '\n');
+	} while(a.annee<0);
 	
 	do{
-	printf("Poids (kg) : ");
-	scanf("%f", &a.poids);
-	}while(a.poids<0);   
+		printf("Poids (kg) : \n");
+		scanf("%f", &a.poids);
+		while (getchar() != '\n');
+	} while(a.poids<=0);   
 
 	int choix;
 	do{
-	printf("Voulez-vous ajouter une description : 1 oui 2 non \n");
-	scanf("%d", &choix);
-	}while(choix<1 || choix>2);
-	if(choix == 1){
-		printf("Description : ");
-		scanf("%s", a.descrip);
-	}
-	afficheAnimale(a);
-	refuge[nb_animal++] = a;
-    }
+		printf("Voulez-vous ajouter une description : 1 oui 2 non \n");
+		scanf("%d", &choix);
+		while (getchar() != '\n');
+	} while(choix!=1 && choix!=2);
+		if (choix == 1){
+    			printf("Description : \n");
+    			fgets(a.descrip, MAX,stdin);
+            		a.descrip[strcspn(a.descrip, "\n")] = '\0';
+		}
+	refuge[nb_animal++] = a;    // incrémenter le nombre d'animal 
+        afficheAnimale(a, choix);
+
+    printf("✅ Animal ajouté avec succès.\n");  
+    sauvegarder_animaux();  // sauvegarder l'ajout
 }
+}
+
+
