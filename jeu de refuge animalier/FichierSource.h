c806c02b64ffb86f6cefedaf6e16e5fbb165ee1f
deusyg <moulayyoussef.ghazi@gmail.com>
2025-05-10 23:37:38 +0200

    Update FichierSource.h

--- jeu de refuge animalier/FichierSource.h
+++ jeu de refuge animalier/FichierSource.h
@@ -7,6 +7,7 @@ void ajouter_animal();
 int RechercheAnimal();
 void afficheAnimale (Animal a);
 void adopter_animal();
+void afficherBanniere();
 void afficherInventaireNbDesc();
 void day_food();
 int calculer_charge_nettoyage_hebdomadaire();
