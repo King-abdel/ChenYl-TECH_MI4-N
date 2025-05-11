a6fa2aa254e3e247d1c149fe641544f3c40e9f40
deusyg <moulayyoussef.ghazi@gmail.com>
2025-05-11 12:41:47 +0200

    Update RechercheAnimal.c

--- jeu de refuge animalier/RechercheAnimal.c
+++ jeu de refuge animalier/RechercheAnimal.c
@@ -1,147 +1,76 @@
 #include "FichierStructure.h"
 #include "FichierSource.h"
 
-Espece ChoisirEspece(){
-    int a;
-    printf("Espèce (1:Chien, 2:Chat, 3:Hamster, 4:Autruche) : ");
-    scanf("%d", &a);
-    while (getchar() != '\n');
-    while(a < 1 || a > 4){
-        scanf("%d", &a);
-        while (getchar() != '\n');
-    }
-    return (Espece)a;  
-}
+// Fonction permettant de rechercher des animaux selon plusieurs critères (nom, espèce, âge)
+void rechercher_animaux() {
+    Animal a;                      // Variable  pour parcourir le tableau
+    char nom[MAX];                // Nom à saisir
+    int filtre_espece, age_type;  // Variables pour filtrer espèce et type d’âge
+    int critere_nom = 0, critere_espece = 0, critere_age = 0; // Variable pour savoir les filtres saisir
 
-int AnneeCourante(){
-    int annee_actuel;
-    time_t ts = time(NULL);    //time_t permet de déclarer une variable en seconde depuis 1970. time(NULL) c'est une fonction qui sert à obtenir le temps actuel en seconde. 
-    annee_actuel = 1970 + (ts/(60*60*24*365));   // on fait la conversion de ts en année puis on ajoute 1970.
-    return annee_actuel;
- } 
-
-void DecoupageTabAge(Animal *enfant[], Animal *jeune[], Animal *senior[], int *e, int *j, int *s) {
-    int i;
-    *e = *j = *s = 0;  
-    int annee_courante = AnneeCourante();
-
-    for (i = 0; i < nb_animal; i++) {  
-        int age = annee_courante - refuge[i].annee;
-
-        if (age < 2) {
-            enfant[*e] = &refuge[i];
-            (*e)++;
-        } else if (age <= 10) {
-            jeune[*j] = &refuge[i];
-            (*j)++;
-        } else {
-            senior[*s] = &refuge[i];
-            (*s)++;
-        }
-    }
-}
-
-int Verification(Animal *tab[], char nom[], int critere_n, int critere_e, int critere_a, int taille, Espece espece){
-    int i;
-    for(i = 0; i< taille; i++){
-        if(critere_n == 1 || critere_e == 1 ){
-            if(strcmp(tab[i]->nom, nom) == 0 && tab[i]->espece == espece ){
-                afficheAnimal(*tab[i], 1);
-                return 0;
-            }
-            else if(critere_e == 0 && strcmp(tab[i]->nom, nom) == 0){
-                afficheAnimal(*tab[i], 1);
-                return 0;
-            }
-             else if(critere_n == 0 && tab[i]->espece == espece){
-                afficheAnimal(*tab[i], 1);
-                return 0;
-            }
-        }
-        else if(critere_a == 1){
-            afficheAnimal(*tab[i], 1);
-            return 0;
-        }
-    }
-    printf("Aucan animal trouvé !\n");
-    return 1;
-}
-
-int Validation(int critere){
-    while(critere != 0 && critere != 1){
-        printf("Choix invalide !");
-        scanf("%d", &critere);
-        while (getchar() != '\n');
-    }
-    return critere;
-}
-
-int RechercheAnimal(){
-    char n[MAX];
-    Espece e;
-    int critere_nom , critere_espece, critere_age, age_type;
-    int a, b, c;
-    Animal *enfant[NbAnimal];  
-    Animal *jeune[NbAnimal];
-    Animal *senior[NbAnimal];
-        
+    // Demande si l'utilisateur veut filtrer par nom
     printf("Rechercher par nom ? (1:Oui / 0:Non) : ");
     scanf("%d", &critere_nom);
-    while (getchar() != '\n');
-    critere_nom = Validation(critere_nom);
+    while (getchar() != '\n');  // supprime '\n' du tampon
     if (critere_nom == 1) {
         printf("Entrez le nom : ");
-        fgets(n, MAX, stdin);                     // lire une chaîne avec des espaces
-        n[strcspn(n, "\n")] = '\0';             // enlève du \n dans la chaîne
+        fgets(nom, MAX, stdin);                     // lire une chaîne avec des espaces
+        nom[strcspn(nom, "\n")] = '\0';             // enlève du \n dans la chaîne
     }
 
     // Demande si l'utilisateur veut filtrer par espèce
     printf("Rechercher par espèce ? (1:Oui / 0:Non) : ");
     scanf("%d", &critere_espece);
     while (getchar() != '\n');
-    critere_espece = Validation(critere_espece);
     if (critere_espece == 1) {
-        e = ChoisirEspece();
+        printf("Espèce (1:Chien, 2:Chat, 3:Hamster, 4:Autruche) : ");
+        scanf("%d", &filtre_espece);
+        while (getchar() != '\n');
     }
 
     // Demande si l'utilisateur veut filtrer par âge
     printf("Rechercher par type d'âge ? (1:Oui / 0:Non) : ");
     scanf("%d", &critere_age);
     while (getchar() != '\n');
-    critere_age = Validation(critere_age);
     if (critere_age == 1) {
-        do{
-        printf("1: Enfant (<2 ans), 2: Jeune (2-10 ans), 3: Sénior (>10 ans) : ");
+        printf("1: Jeune (<2 ans), 2: Senior (>10 ans) : ");
         scanf("%d", &age_type);
         while (getchar() != '\n');
-        }while( age_type < 1 || age_type > 3);
     }
 
-    DecoupageTabAge(enfant, jeune, senior, &a, &b, &c);
+    int trouve = 0;                // Marqueur pour savoir si un animal a été trouvé
+    int annee_courante = 2025;     // Année actuelle pour le calcul de l’âge
 
-    if(critere_age == 0){
-        if(critere_espece == 1 || critere_nom == 1){
-            Animal *ptrRefuge[NbAnimal];
-            for (int i = 0; i < nb_animal; i++) {
-                ptrRefuge[i] = &refuge[i];
-            }
-        Verification(ptrRefuge, n, critere_nom, critere_espece, critere_age, nb_animal, e);
+    // Parcours de tous les animaux du refuge
+    for (int i = 0; i < nb_animal; i++) {
+        a = refuge[i];
+        int age = annee_courante - a.annee;  // Calcul de l’âge de l’animal
+
+        int repere = 1;  //vérifie si l’animal correspond à tous les filtres
+
+        // Filtre par nom 
+        if (critere_nom == 1 && strcmp(a.nom, nom) != 0)
+            repere = 0;
+
+        // Filtre par espèce
+        if (critere_espece == 1 && a.espece != filtre_espece)
+            repere = 0;
+
+        // Filtre par type d’âge 
+        if (critere_age == 1) {
+            if ((age_type == 1 && age >= 2) || (age_type == 2 && age <= 10))
+                repere = 0;
         }
-        else{
-            printf("Aucun critère de recherche sélectionné. Recherche impossible.\n");
+
+        // Si l’animal vérifie les filtres, on l’affiche
+        if (repere == 1) {
+            afficheAnimale(a, 1);  // Affichage des caractéristiques de l'animal
+            trouve = 1;
         }
     }
-        
-    else if(critere_age == 1){
-        if(age_type == 1){
-            Verification(enfant, n, critere_nom, critere_espece, critere_age, a, e);
-        }
-        else if(age_type == 2){
-            Verification(jeune, n, critere_nom, critere_espece, critere_age,b, e);
-        }
-        else if(age_type == 3){
-            Verification(senior, n, critere_nom, critere_espece, critere_age,c, e);
-        }
-    }	
-    return 1;
+
+    // le cas où aucun animal n’a été trouvé
+    if (trouve == 0){
+        printf("Aucun animal ne correspond aux critères.\n");
+    }
 }
